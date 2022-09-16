fc51f9a6fef677e658143e119f9586606b7846c4
Michaelu670 <michaelu670@gmail.com>
2022-09-16 19:07:18 +0700

    first commit

---
+++ src/driver/Driver.java
@@ -0,0 +1,13 @@
+package driver;
+
+import matriks.Matriks;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Matriks m = new Matriks();
+		m.print();
+	}
+
+}
---
+++ src/matriks/Matriks.java
@@ -0,0 +1,10 @@
+package matriks;
+
+public class Matriks {
+	double[][] mat = null;
+	int rowCnt, colCnt;
+	
+	public void print() {
+		System.out.println("A");
+	}
+}
