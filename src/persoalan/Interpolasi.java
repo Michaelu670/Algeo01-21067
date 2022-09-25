58f3f181b38eca35b90c9c6b10c88f0364233ecc
Febryan Arota <88846095+febryanarota@users.noreply.github.com>
2022-09-25 08:31:43 +0700

    add interpolasi

--- src/persoalan/Interpolasi.java
+++ src/persoalan/Interpolasi.java
@@ -1,5 +1,27 @@
 package persoalan;
 
 public class Interpolasi {
+  
+	public float interpolasi(double x, Matriks m ) {
+		Matriks mtx = new Matriks(m.getRow(), m.getRow()+1);
+		float ans = 0;
+		
+		for (int i = 0; i < mtx.getRow(); i++) {
+			for (int j = 0; j < mtx.getCol(); j++) {
+				if (j == mtx.getCol()-1) {
+					mtx.setMat(i, j, m.getMat(i, j));
+				} else {
+					mtx.setMat(i, j, Math.pow(m.getMat(i, 0), j));
+				}
+			}
+		}
+		
+		mtx = mtx.cramer();
+		
+		for (int i = 0; i< mtx.getRow(); i++) {
+			ans += mtx.getMat(i, 0) * Math.pow(x, i);
+		}
+		return ans;
+	}
 
 }
